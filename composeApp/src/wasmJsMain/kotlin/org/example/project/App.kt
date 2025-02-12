681e346c9f5c5a1bea8b12f189c383f3c704567b
kei-1111 <kei_02_1111@icloud.com>
2025-02-12 22:19:23 +0900

    first commit

---
+++ composeApp/src/wasmJsMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,75 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import io.github.koalaplot.core.bar.BulletGraphs
+import io.github.koalaplot.core.style.KoalaPlotTheme
+import io.github.koalaplot.core.style.KoalaPlotTheme.axis
+import io.github.koalaplot.core.util.ExperimentalKoalaPlotApi
+import io.github.koalaplot.core.xygraph.FloatLinearAxisModel
+import org.jetbrains.compose.resources.painterResource
+import research_system.composeapp.generated.resources.Res
+import research_system.composeapp.generated.resources.compose_multiplatform
+import kotlin.text.Typography.bullet
+
+@Composable
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalKoalaPlotApi::class)
+@Composable
+fun GraphSample() {
+    MaterialTheme {
+        Column {
+            Text(
+                text = "Hello, World!",
+            )
+        }
+        BulletGraphs(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            bullet(FloatLinearAxisModel(0f..300f)) {
+                label {
+                    Column(
+                        horizontalAlignment = Alignment.End,
+                        modifier = Modifier.padding(end = KoalaPlotTheme.sizes.gap)
+                    ) {
+                        Text("Revenue 2005 YTD", textAlign = TextAlign.End)
+                        Text("(US $ in thousands)", textAlign = TextAlign.End)
+                    }
+                }
+                axis { labels { Text("${it.toInt()}") } }
+                comparativeMeasure(260f)
+                featuredMeasureBar(275f)
+                ranges(0f, 200f, 250f, 300f)
+            }
+        }
+    }
+}
