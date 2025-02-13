440d03937cad5824fc09545739c65c8dcaeb30a0
kei-1111 <kei_02_1111@icloud.com>
2025-02-14 00:12:58 +0900

    add: 人口の統計データをもとにグラフを表示するよう変更

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -39,7 +39,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation("io.github.koalaplot:koalaplot-core:0.8.0")
+            implementation(libs.chart)
         }
     }
 }
--- composeApp/src/wasmJsMain/kotlin/org/example/project/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/example/project/Main.kt
@@ -3,10 +3,11 @@ package org.example.project
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.example.project.ui.App
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        GraphSample()
+        App()
     }
 }
